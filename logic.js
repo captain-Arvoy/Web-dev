29df98761c22cb8c6b326b0ef8e57740d4da325d
captain-Arvoy <accofstardust@gmail.com>
2023-06-30 21:55:22 +0530

    Done, ready to deploy

--- logic.js
+++ logic.js
@@ -1,16 +1,17 @@
 var userString;
+let str = '';
 const button = document.addEventListener("click",()=>{
   const userText = document.querySelector("#userInput");
   console.log(userText.value);
   userString = userText.value.split(' ');
   userString.forEach((element) => {
-    console.log(element+"\n"+element.length);
     strArray = element.split('');
     let sum = 0;
     for (let i=element.length - 1;i >= 0; i--){
-      console.log("i = "+i)
-      sum += parseInt(strArray[i])*Math.pow(2,i)*Math.pow(10,i);
-    console.log("element decimal = "+sum)
+      sum += parseInt(strArray[i])*Math.pow(2,element.length-1-i);
     }
+    // console.log("sum = "+sum+"\nchar = "+String.fromCharCode(sum));
+    str = str + String.fromCharCode(sum);
   });
+  console.log(str)
 })
